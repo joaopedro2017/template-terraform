3e1f612183264ad7c17a8fa360a94901fa32df33
JoaoPresto <jguedespresto@gmail.com>
2023-04-23 16:36:10 -0300

    Create provider Azure

---
+++ module/template/azure/variables.tf
@@ -0,0 +1,14 @@
+variable "company" {
+    type = string
+    default = "none"
+}
+
+variable "name" {
+   type    = string
+  default = "none"
+}
+
+variable "location" {
+  type    = string
+  default = "none"
+}
